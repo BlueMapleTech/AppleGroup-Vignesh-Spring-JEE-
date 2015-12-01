300d0d4640ca75dc74127f6bde658ef8a1866d43
vigneshpandi <vignesh08993@gmail.com>
2015-11-30 21:48:04 -0800

    Revert "TicketReservation2"
    This reverts commit 779701c74fda67b66d64864fdd5fb680c823dcc3.

--- TicketReservation2/src/main/java/com/bluemaple/ticketreservation/dao/LoginDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bluemaple.ticketreservation.dao;
-
-import com.bluemaple.ticketreservation.model.User;
-
-public interface LoginDaoInterface {
-
-public	User login(String name1);
-
-}
--- TicketReservation2/src/main/java/com/bluemaple/ticketreservation/service/LoginServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bluemaple.ticketreservation.service;
-
-import com.bluemaple.ticketreservation.model.User;
-
-public interface LoginServiceInterface {
-
-public	User login(User user);
-
-}
